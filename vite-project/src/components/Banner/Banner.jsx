ea9ba6739984c725be4884a15814131b49199a9d
Your Name <adnan.mohamm2022@gmail.com>
2025-02-19 19:59:17 -0500

    banner

---
+++ vite-project/src/components/Banner/Banner.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import axios from "../../utils/axios";
+import requests from "../../utils/requests";
+import "./banner.css"
+
+const Banner = () => {
+  const [movie, setMovie] = useState({});
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        console.log(request);
+        setMovie(
+          request.data.results[
+            Math.floor(Math.random() * request.data.results.length)
+          ]
+        );
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, []);
+  const truncate = (str, maxLength) => {
+    return str?.length > maxLength ? `${str.substr(0, maxLength - 1)}…` : str;
+  };
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner__fadeBottom" />
+    </div>
+  );
+};
+
+export default Banner;
+
+// console.log(import.meta.env.VITE_API_KEY);
+
